48ba7f2026a9a3079ab6e4292d3363b8146d3abe
luisDogo <luisdogoms@gmail.com>
2024-10-25 07:20:07 -0600

    ruben

--- language_model_creation.py
+++ language_model_creation.py
@@ -16,12 +16,16 @@ def main():
     n = 4
 
     # corpora creation
-
-    df = pd.read_csv("corpora/LDMS.csv")
+    ldms = pd.read_csv("corpora/LDMS.csv")
+    rrm = pd.read_csv("corpora/RubenChatsJoined.csv").dropna(subset=["content"])
+    # eyag = pd.read_csv()
+    # dahb = pd.read_csv()
     corpora = {
-        "Moreno_Santamaria_Luis_Daniel" : df["content"]
+        "Moreno_Santamaria_Luis_Daniel" : ldms["content"],
+        "Ruben_Rodriguez_Mendez" : rrm["content"],
+        "Edwin_Yahir_Arteaga_Gonzalez" : 0,
+        "Diego_Alberto_Hernandez_Barrera" : 0
     }
-
     for alumno in corpora:
         # .csv creation
         for ngram in range(2,n):
@@ -30,7 +34,6 @@ def main():
             ft_matrix = vectorizer.fit_transform(corpora[alumno])
             df_ngram = pd.DataFrame(ft_matrix) 
             df_ngram.to_csv(ngrams_file_name, sep = "\t")
-
     return 0
 
 
