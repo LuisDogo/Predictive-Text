6aeebbf9cceb99b78a9f6b5661b049cacdd0d51f
luisDogo <luisdogoms@gmail.com>
2024-10-24 19:38:17 -0600

    comments erased

--- language_model_creation.py
+++ language_model_creation.py
@@ -38,17 +38,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# if(os.path.exists(vsm_file_name)):
-#     print('Ya existe')
-#     vsm_file = open(vsm_file_name,'rb')
-#     vsm = pickle.load(vsm_file)
-# else:
-#     norm_df = df_norm(contenido, 6)
-#     vsm = vector_space_model(norm_df, contexto, unit_vector)
-#     vsm_file = open(vsm_file_name,'wb')
-#     pickle.dump(vsm, vsm_file)
-#     vsm_file.close()
-#     ft_vsm_file = open(ft_vsm_file_name,'wb')
-#     pickle.dump(vsm.ft_matrix, ft_vsm_file)
-#     ft_vsm_file.close()
